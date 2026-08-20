2c7bfced250ddba6d952dcb157fe2864eb2acfde
896136530 <896136530@qq.com>
2026-08-20 22:31:22 +0800

    Day4 完成第一题

--- day3/Day3_Ex3.java
+++ day3/Day3_Ex3.java
@@ -1,16 +1,16 @@
 public class Day3_Ex3 {
     public static void main(String[] args) {
         // 题 3：把数组原地反转（不新建数组），最终打印 6 5 4 3 2 1
-        int[] arr = {1, 2, 3, 4, 5, 6};
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
         // 提示：arr[i] 和 arr[arr.length - 1 - i] 交换，i 走到 length/2 即可
 
         // ===== 你的代码写在这里 =====
-for(int i=0;i<=arr.length/2-1;i++){
-    int temp=0;
-    temp=arr[i];
-    arr[i]=arr[arr.length-1-i];
-    arr[arr.length-1-i]=temp;
-}
+        for (int i = 0; i <= arr.length / 2 - 1; i++) {
+            int temp = 0;
+            temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
         // ===========================
 
         // 打印结果（应输出 6 5 4 3 2 1）
--- day3/Day3_Ex5.java
+++ day3/Day3_Ex5.java
@@ -6,21 +6,21 @@ public class Day3_Ex5 {
 
         // ===== 你的代码写在这里（用数组存 5 个成绩，再遍历统计）=====
         // 输出示例："及格人数：3，不及格人数：2"
-Scanner sc = new Scanner(System.in);
-int score[]= new int[5];
-for(int i=0;i<5;i++){
-score[i]=sc.nextInt();
-}
-int x=0;int y=0;
-for(int i=0;i<5;i++){
-    if(score[i]>=60){
-x++;
-    }
-    else{
-        y++;
-    }
-}
-System.out.println("及格人数："+x+",不及格人数："+y);
+        Scanner sc = new Scanner(System.in);
+        int score[] = new int[5];
+        for (int i = 0; i < 5; i++) {
+            score[i] = sc.nextInt();
+        }
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < 5; i++) {
+            if (score[i] >= 60) {
+                x++;
+            } else {
+                y++;
+            }
+        }
+        System.out.println("及格人数：" + x + ",不及格人数：" + y);
         // ===========================================================
         sc.close();
     }
